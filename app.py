263ec362e6b483662b2734b00164929908bcafa7
Rishabh Jha <jha112000rishabh@gmail.com>
2024-06-05 23:08:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!, My name is Rishabh'
+    return 'Hello, world!, My name is Rishabh'    
 
 if __name__ == '__main__':
     app.run()
