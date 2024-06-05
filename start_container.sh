f7ccb521b56b182ab3a3dd42c86d91961ce211cb
Rishabh Jha <jha112000rishabh@gmail.com>
2024-06-05 22:32:20 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the docker image from the docker hub
-docker pull rj972/sample-python-flask-project
+docker pull rj972/sample-python-flask-project  
 
 # Run the docker image as a container
 docker run -d -p 5000:5000 rj972/sample-python-flask-project
